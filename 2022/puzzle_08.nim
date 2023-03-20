b70515da19b45a735257ecb86d547aa651b9c21e
Eric N. Vander Weele <ericvw@gmail.com>
2023-03-20 16:19:48 -0400

    2022: Day 8

---
+++ 2022/puzzle_08.nim
@@ -0,0 +1,64 @@
+import std/sequtils
+import std/sugar
+
+type
+    TreeHeight = int
+    TreeRow = seq[TreeHeight]
+    TreeMap = seq[TreeRow]
+
+let treeMap: TreeMap = collect:
+    for line in stdin.lines:
+        line.map((h) => h.ord - '0'.ord)
+
+let
+    ROWS = treeMap.len
+    COLS = treeMap[0].len
+
+proc isEdge(x, y: int): bool =
+    x - 1 < 0 or x + 1 >= ROWS or
+    y - 1 < 0 or y + 1 >= COLS
+
+proc visibleFromEdge(tm: TreeMap, x, y: int): bool =
+    proc isVisible(tm: TreeMap, h, x, y, xStep = 0, yStep = 0): bool =
+        if isEdge(x, y):
+            return true
+        elif h - treeMap[x + xStep][y + yStep] > 0:
+            return isVisible(tm, h, x + xStep, y + yStep, xStep, yStep)
+        else:
+            return false
+
+    let h = tm[x][y]
+    return isVisible(tm, h, x, y, xStep = 1) or
+           isVisible(tm, h, x, y, xStep = -1) or
+           isVisible(tm, h, x, y, yStep = 1) or
+           isVisible(tm, h, x, y, yStep = -1)
+
+var nVisibile = 0
+for r in 0 ..< ROWS:
+    for c in 0 ..< COLS:
+        if visibleFromEdge(treeMap, r, c):
+            inc(nVisibile)
+
+echo "Part 1: ", nVisibile
+
+proc scenicScore(tm: TreeMap, x, y: int): int =
+    proc viewDistance(tm: TreeMap, h, x, y, xStep = 0, yStep = 0): int =
+        if isEdge(x, y):
+            return 0
+        elif h - treeMap[x + xStep][y + yStep] > 0:
+            return 1 + viewDistance(tm, h, x + xStep, y + yStep, xStep, yStep)
+        else:
+            return 1
+
+    let h = tm[x][y]
+    return viewDistance(tm, h, x, y, xStep = 1) *
+           viewDistance(tm, h, x, y, xStep = -1) *
+           viewDistance(tm, h, x, y, yStep = 1) *
+           viewDistance(tm, h, x, y, yStep = -1)
+
+var maxScenicScore = 0
+for r in 0 ..< ROWS:
+    for c in 0 ..< COLS:
+        maxScenicScore = max(scenicScore(treeMap, r, c), maxScenicScore)
+
+echo "Part 2: ", maxScenicScore
