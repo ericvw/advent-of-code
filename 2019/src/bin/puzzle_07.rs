e1404e1455568905faa818a9364551c9224861be
Eric N. Vander Weele <ericvw@gmail.com>
2024-03-21 13:56:47 -0400

    2019: Day 7

---
+++ 2019/src/bin/puzzle_07.rs
@@ -0,0 +1,10 @@
+use std::io;
+
+use aoc2019::intcode;
+
+fn main() {
+    let amplifier_controller_program = intcode::parse_program(io::stdin());
+
+    println!( "Part 1: {:?}", ());
+    println!( "Part 2: {:?}", ());
+}
