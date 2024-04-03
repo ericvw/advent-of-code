f4d06756c868bdf0cd005038fa6ce30e52423e5d
Eric N. Vander Weele <ericvw@gmail.com>
2024-04-03 16:48:50 -0400

    2019: Day 7

---
+++ 2019/src/bin/puzzle_07.rs
@@ -0,0 +1,44 @@
+use std::io;
+
+use aoc2019::intcode;
+
+fn run_amplifier_loop(program: &[i32], phase_settings: &[i32]) -> i32 {
+    let mut amplifiers: Vec<_> = phase_settings
+        .iter()
+        .map(|&phase_setting| intcode::Computer::new(program, &[phase_setting]))
+        .collect();
+
+    let mut signal = 0;
+
+    'feedback_loop: loop {
+        for a in &mut amplifiers {
+            a.input.push_back(signal);
+            signal = match a.run() {
+                intcode::State::Output(x) => x,
+                intcode::State::Halt => break 'feedback_loop,
+            };
+        }
+    }
+
+    signal
+}
+
+fn main() {
+    let amplifier_controller_program = intcode::parse_program(io::stdin());
+
+    println!(
+        "Part 1: {}",
+        aoc2019::iter::permutation(&[0, 1, 2, 3, 4])
+            .map(|x| run_amplifier_loop(&amplifier_controller_program, &x))
+            .max()
+            .unwrap()
+    );
+
+    println!(
+        "Part 2: {}",
+        aoc2019::iter::permutation(&[5, 6, 7, 8, 9])
+            .map(|x| run_amplifier_loop(&amplifier_controller_program, &x))
+            .max()
+            .unwrap()
+    );
+}
